474ce4156602ad18988f7fc9d080e9d7c087dfc0
will-chrizt <131515795+will-chrizt@users.noreply.github.com>
2025-08-13 18:22:09 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,39 +2,12 @@ provider "aws" {
   region = var.aws_region
 }
 
-resource "aws_security_group" "k8s_master_sg" {
-  name        = "k8s-master-sg"
-  description = "Allow SSH and Kubernetes API"
-
-  ingress {
-    description = "SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "Kubernetes API"
-    from_port   = 6443
-    to_port     = 6443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
 
 resource "aws_instance" "k8s_master" {
   ami                    = "ami-04f59c565deeb2199"
   instance_type          = var.instance_type
   key_name               = var.ssh_key_name
-  vpc_security_group_ids = [aws_security_group.k8s_master_sg.id]
+
   associate_public_ip_address = true
 
   user_data = file("./bootstrap.sh")
