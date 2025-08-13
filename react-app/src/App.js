d0d1cb0984ef6a2cb5b5d3e4331f7a4c6869f9d0
will-chrizt <131515795+will-chrizt@users.noreply.github.com>
2025-08-13 12:10:28 +0530

    Create App.js

---
+++ react-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Welcome to My React App</h1>
+      <p>This app is running in Kubernetes!</p>
+    </div>
+  );
+}
+
+export default App;
