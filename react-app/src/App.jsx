d5413b4e8832ea788865b7a591761ee8c18ccbfd
will-chrizt <131515795+will-chrizt@users.noreply.github.com>
2025-08-14 11:01:52 +0530

    Update App.jsx

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -4,7 +4,7 @@ import './styles.css';
 function App() {
   return (
     <div className="container">
-      <h1>It works</h1>
+      <h1>It works.</h1>
       <p>This app is running in Kubernetes!</p>
       <p>
         It only took 47 `kubectl apply`s, 3 existential crises.
