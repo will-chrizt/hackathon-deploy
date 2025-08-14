58753a648a34e6dec50a6c631948c0139f62a525
will-chrizt <131515795+will-chrizt@users.noreply.github.com>
2025-08-14 09:02:46 +0530

    Update App.jsx

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
       <h1>Welcome to My React App</h1>
       <p>This app is running in Kubernetes!</p>
       <p>
-        It only took 47 `kubectl apply`s, 3 existential crises, and a NodePort that ghosted me harder than my last date.
+        It only took 47 `kubectl apply`s, 3 existential crises.
       </p>
       <p>
         But hey—if you're reading this, the YAML gods have finally smiled upon us.
