047ee25670c4b9a06d57394f5530e9066818265e
will-chrizt <131515795+will-chrizt@users.noreply.github.com>
2025-08-14 09:11:05 +0530

    Update App.jsx

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
   return (
     <div className="container">
-      <h1>Welcome to My React App</h1>
+      <h1>It worked</h1>
       <p>This app is running in Kubernetes!</p>
       <p>
         It only took 47 `kubectl apply`s, 3 existential crises.
